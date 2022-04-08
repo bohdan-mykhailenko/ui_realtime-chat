af0e0594885680f739e79764c7d1e01a27afb9fe
Bohdan <6alfabravo9@gmail.com>
2022-04-08 18:47:54 +0300

    date

--- react-chat/src/components/home/Home.jsx
+++ react-chat/src/components/home/Home.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classes from './Home.module.css'
-import Loader from '../loader/Loader'
-import { Grid, Container, Button } from '@material-ui/core';
-import Typography from '@mui/material/Typography';
+import { Grid, Container } from '@material-ui/core';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import ExtensionIcon from '@mui/icons-material/Extension';
 import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
 import { ReactComponent as ReactIcon } from '../../imgs/react.svg';
@@ -15,7 +11,7 @@ import { ReactComponent as ReactIcon } from '../../imgs/react.svg';
 
 
 const Home = () => {
-  const techArray = ['Java Sctipt(ES6)', 'React 18.0.0', 'MaterialUl 5.5.3', 'Firebase 9.6.10', 'HTML5', 'CSS3'];
+  const techArray = ['Java Sctipt(ES6)', 'React 18.0.0', 'MaterialUl 5.5.3(framework)', 'Firebase 9.6.10(service)', 'HTML5', 'CSS3(animation)'];
   const abilArray = ['Authentication with Google', 'Chating in realtime', 'Sending an atachments', 'Typing emodji', 'Changing UI theme', 'Watching the time'];
 
   function CreateList(props) {
@@ -51,25 +47,22 @@ const Home = () => {
           The idea to create this program came from watching a video from Ulbi (YouTube). The author showed the work of React(JavaScript library) and Firebase  - popular service for hosting apps and actions with databases, and it was so interesting and exciting for me because of my desire to study Web-development.
           I've been studying React for 2 weeks, so these materials helped me to find new information and improve my skills with this library.
         </Grid>
-        <Grid
-          container
-          className={classes.lists}>
-          <Grid>
-            <h2 className={classes.subtitle}>Technologies:
+        <div className={classes.lists}>
+          <Grid className={classes.firstList}>
+            <h2 className={classes.subtitle}>Abilities:
             </h2>
-            <CreateList array={techArray} icons={1} />
+            <CreateList array={abilArray} icons={1} />
           </Grid>
           <Grid className={classes.wrapperReactIcon} >
             <ReactIcon className={classes.reactIcon} />
           </Grid>
           <Grid>
-            <h2 className={classes.subtitle}>Abilities:
+            <h2 className={classes.subtitle}>Technologies:
             </h2>
-            <CreateList array={abilArray} icons={2} />
+            <CreateList array={techArray} icons={2} />
           </Grid>
-        </Grid>
+        </div>
         <Grid className={classes.enjoy}>Enjoy with using my app😊</Grid>
-
       </Grid>
     </Container>
   )
